22f91b07be02ce18ec93086f9b6fee88f17ca426
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-13 18:32:36 -0500

    File Definitions added.

--- server/app/config.js
+++ server/app/config.js
@@ -1,4 +1,7 @@
 // Provides config for the app in conjunction with dotenv
+// All changes made to anything data, must be done here to help keep track of all trade.
+
+
 // Config is the only place accesses 'dotenv'
 import dotenv from "dotenv";
 
--- server/app/controller.js
+++ server/app/controller.js
@@ -1,1 +1,2 @@
 // Uses the db client from loader.js
+// The controller controls all access to your Database.
