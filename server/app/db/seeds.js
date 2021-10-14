140fa5ddf412b99c88a26c765242855ee90423cd
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-13 20:22:43 -0500

    Seeded the database using code

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,5 +1,11 @@
 import once from "./connections/once.js";
+import productsData from "./data.js";
 
-once.connect().then((connections) => {
-  console.log(connections);
-});
+once
+  .connect()
+  .then((connections) =>
+    connections.db("products").collection("products").insertMany(productsData)
+  )
+  .then(() => {
+    once.close();
+  });
