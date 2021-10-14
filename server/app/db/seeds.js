b63357b2f6a6a67a7a568ec1911d3335d2ef6b4d
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-13 20:49:20 -0500

    Change to async await to clear and reloag data.

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,11 +1,19 @@
 import once from "./connections/once.js";
 import productsData from "./data.js";
 
-once
-  .connect()
-  .then((connection) =>
-    connection.db("products").collection("products").insertMany(productsData)
-  )
-  .then(() => {
-    once.close();
-  });
+// Created an (Anonymous IIFE)--- Immediately Invoke Function Expression --- TO USE ASYNCO AWAIT, you have to use and IIFE or a
+(async () => {
+  const conn = await once.connect();
+  await conn.db("products").collection("products").deleteMany({});
+  await conn.db("products").collection("products").insertMany(productsData);
+  conn.close();
+})();
+
+// once
+//   .connect()
+//   .then((connection) =>
+//     connection.db("products").collection("products").insertMany(productsData)
+//   )
+//   .then(() => {
+//     once.close();
+//   });
