e94250114573394d4ca0aae498fcbf93ac472d13
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-23 22:15:57 -0500

    Update seeds grenerator to universal nameing .

--- server/app/db/seeds.js
+++ server/app/db/seeds.js
@@ -1,13 +1,6 @@
+import config from "../config.js";
 import once from "./connections/once.js";
 import productsData from "./data.js";
-
-// Created an (Anonymous IIFE)--- Immediately Invoke Function Expression --- TO USE ASYNCO AWAIT, you have to use and IIFE or a
-(async () => {
-  const conn = await once.connect();
-  await conn.db("products").collection("products").deleteMany({});
-  await conn.db("products").collection("products").insertMany(productsData);
-  conn.close();
-})();
 
 // once
 //   .connect()
@@ -17,3 +10,22 @@ import productsData from "./data.js";
 //   .then(() => {
 //     once.close();
 //   });
+
+// Created an (Anonymous IIFE)--- Immediately Invoke Function Expression --- TO USE ASYNCO AWAIT, you have to use and IIFE or a
+// (async () => {
+//   const conn = await once.connect();
+//   await conn.db("products").collection("products").deleteMany({});
+//   await conn.db("products").collection("products").insertMany(productsData);
+//   conn.close();
+// })();
+
+const {
+  db: { name, collectionName: collection },
+} = config;
+
+(async () => {
+  const conn = await once.connect();
+  await conn.db(name).collection(collection).deleteMany({});
+  await conn.db(name).collection(collection).insertMany(productsData);
+  conn.close();
+})();
