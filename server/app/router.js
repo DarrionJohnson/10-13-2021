f8b7705fc79ba81eee95e59ed365fa0855c926b3
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-10-24 20:49:30 -0500

    Update find by id by var repeat code.

--- server/app/router.js
+++ server/app/router.js
@@ -5,34 +5,27 @@ import { ObjectId } from "mongodb";
 import config from "./config.js";
 import client from "./db/connections/client.js";
 
+const collection = client
+  .db(config.db.name)
+  .collection(config.db.collectionName);
 const router = new Router();
-const {
-  db: { name, collectionName: collection },
-} = config;
 
-console.log(name, collection);
-
+// localhost:3000/api
 router.get("/", (_, res) => {
   res.send("Hello From API router!");
 });
 
+// localhost:3000/api/products
 // REMEMBER: You need a toArray for it to format correctly.
 router.get("/products", async (_, res) => {
   console.log(client.db(name).collection(collection));
-  const products = await client
-    .db(name)
-    .collection(collection)
-    .find({})
-    .toArray();
+  const products = await collection.find({}).toArray();
   res.json(products);
 });
 
 // : (colon) makes this code dynamic, or changeable.
 router.get("/products/:id", async (req, res) => {
-  const product = await client
-    .db(name)
-    .collection(collection)
-    .findOne({ _id: ObjectId(req.params.id) });
+  const product = await collection.findOne({ _id: ObjectId(req.params.id) });
   res.json(product);
 });
 
